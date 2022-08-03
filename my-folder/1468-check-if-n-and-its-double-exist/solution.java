dfdd414a8231a5a3150a9d8d47c52f9da72b33b9
Sanskar Goel <sanskargoel6@gmail.com>
2022-08-03 01:49:38 +0000

    [LeetCode Sync] Runtime - 3 ms (35.49%), Memory - 42.9 MB (29.90%)

---
+++ my-folder/1468-check-if-n-and-its-double-exist/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        boolean res=false;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if((arr[i]==(arr[j]*2)) || (arr[j]==(arr[i]*2)))
+                {
+                    res=true;
+                    break;
+                }
+            }
+            if(res==true)
+            {
+                break;
+            }
+            
+        }
+        return res;
+        
+    }
+}
