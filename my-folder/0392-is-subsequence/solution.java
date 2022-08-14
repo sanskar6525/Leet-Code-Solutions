b028b2a6ce5a292c0a653ca09c613bebf312a6e7
Sanskar Goel <sanskargoel6@gmail.com>
2022-08-14 17:58:50 +0000

    [LeetCode Sync] Runtime - 2 ms (61.86%), Memory - 42 MB (5.38%)

---
+++ my-folder/0392-is-subsequence/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        int p = 0;
+        for (int i = 0; i < t.length(); i++ ) {
+            if (s.charAt(p) == t.charAt(i)) {
+                p++;
+            }
+            if (p == s.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
