e0fa9c8997a065bd811f4b0190299c79ac5336d8
Sanskar Goel <sanskargoel6@gmail.com>
2022-08-20 16:59:33 +0000

    [LeetCode Sync] Runtime - 2 ms (16.28%), Memory - 44.7 MB (5.59%)

--- my-folder/0098-validate-binary-search-tree/solution.java
+++ my-folder/0098-validate-binary-search-tree/solution.java
@@ -1,13 +1,29 @@
 class Solution {
+    class BSTpair{
+        long min;
+        long max;
+        boolean isBST;
+    }
     public boolean isValidBST(TreeNode root) {
-       
-        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        BSTpair p = check(root);
+        return p.isBST;
     }
     
-    public boolean isValidBST(TreeNode root,long minval,long maxval){
-        if(root == null) return true;
-        if(root.val >= maxval || root.val <= minval) return false;
+    public BSTpair check(TreeNode root){
+        if(root == null){
+            BSTpair bp = new BSTpair();
+            bp.min = Long.MAX_VALUE;
+            bp.max = Long.MIN_VALUE;
+            bp.isBST = true;
+            return bp;
+        }
+        BSTpair lp = check(root.left);
+        BSTpair rp = check(root.right);
         
-        return isValidBST(root.left,minval,root.val) && isValidBST(root.right,root.val,maxval);
+        BSTpair mp = new BSTpair();
+        mp.min = Math.min(root.val, Math.min(lp.min, rp.min));
+        mp.max = Math.max(root.val, Math.max(lp.max, rp.max));
+        mp.isBST = lp.isBST && rp.isBST && root.val > lp.max && root.val < rp.min;
+        return mp;
     }
 }
