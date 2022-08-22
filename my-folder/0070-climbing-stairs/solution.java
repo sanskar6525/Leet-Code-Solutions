f7af41194b4e9abae78d28cf3473d9667290019c
Sanskar Goel <sanskargoel6@gmail.com>
2022-08-22 17:06:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (8.11%)

---
+++ my-folder/0070-climbing-stairs/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public int climbStairs(int n) {
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        
+        if(n <= 2){
+            return n;
+        }
+        
+        arr[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+     return arr[n]; 
+    }
+}
