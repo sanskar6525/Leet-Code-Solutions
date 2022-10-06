a47dd85546a60b26249557c226e9281f9634722d
Sanskar Goel <sanskargoel6@gmail.com>
2022-10-06 13:39:57 +0000

    [LeetCode Sync] Runtime - 10 ms (5.25%), Memory - 64.5 MB (5.66%)

--- my-folder/0189-rotate-array/solution.java
+++ my-folder/0189-rotate-array/solution.java
@@ -1,17 +1,25 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k%n;
-        reverse(nums, 0, n-1);
-        reverse(nums, 0, k-1);
-        reverse(nums, k, n-1);
+   public void rotate(int[] nums, int k) {
+        k%=nums.length;
+        int n = nums.length-1;
+        if(n==0){
+            return;
+        }
+        // for reversig the whole array
+        reverse(nums,0,n);
+        // for reversing the first k elements
+        reverse(nums,0,k-1);
+        // for reversing the last elements
+        reverse(nums,k,n);
     }
-    
-    public void reverse(int[] nums, int st, int end) {
-        for(int i=st; st<end; st++,end--) {
-            int temp = nums[st];
-            nums[st] = nums[end];
-            nums[end] = temp;
+    // simple reversing code using two pointer
+    public void reverse(int[] nums , int i ,int j){
+        while(i<=j){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
     }
 }
