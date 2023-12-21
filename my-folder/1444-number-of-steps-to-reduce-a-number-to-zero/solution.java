53654d57cb5eb9660d1948f472524cfaa54debfc
Sanskar Goel <sanskargoel6@gmail.com>
2023-12-21 09:03:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (20.96%)

--- my-folder/1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
+++ my-folder/1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int steps=0;
-        while(num!=0)
-        {
+        if(num==0)
+            return 0;
+        else
             if(num%2==0)
-            {
-                num=num/2;
-                steps++;
-            }
+                return 1+numberOfSteps(num/2);
             else
-            {
-                num=num-1;
-                steps++;
-            }
-        }
-        return steps;
+                return 1+numberOfSteps(num-1);
+        
     }
 }
