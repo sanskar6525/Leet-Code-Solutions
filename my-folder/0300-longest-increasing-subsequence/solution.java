9afd0e342e9c6eb4779f6ea8c2e19a05944257cf
Sanskar Goel <sanskargoel6@gmail.com>
2024-01-05 05:30:30 +0000

    [LeetCode Sync] Runtime - 43 ms (50.15%), Memory - 43.9 MB (68.53%)

--- my-folder/0300-longest-increasing-subsequence/solution.java
+++ my-folder/0300-longest-increasing-subsequence/solution.java
@@ -1,21 +1,22 @@
-class Solution {
+public class Solution {
     public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
-    
-    int result = 1;
-    int[] dp = new int[nums.length ];
-    Arrays.fill(dp, 1);
-    for(int i = 0; i < nums.length; i++) {
-        for(int j = 0; j < i; j++) {
-            
-            if(nums[i] > nums[j]) {
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-                result = Math.max(result, dp[i]);
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
             }
         }
-    }
 
-return result;
+        int maxLength = Arrays.stream(dp).max().orElse(0);
+        return maxLength;
+    }
 }
-    }
-
