d37034e4578b849400a98f277b347fbb8391ec6c
Sanskar Goel <sanskargoel6@gmail.com>
2024-02-05 15:11:23 +0000

    [LeetCode Sync] Runtime - 37 ms (17.34%), Memory - 45.6 MB (5.77%)

--- my-folder/0387-first-unique-character-in-a-string/solution.java
+++ my-folder/0387-first-unique-character-in-a-string/solution.java
@@ -1,20 +1,25 @@
 class Solution {
-     public int firstUniqChar(String s) {
-        // Stores lowest index / first index
-        int ans = Integer.MAX_VALUE;
-        // Iterate from a to z which is 26 which makes it constant
-        for(char c='a'; c<='z';c++){
-            // indexOf will return first index of alphabet and lastIndexOf will return last index
-            // if both are equal then it has occured only once.
-            // through this we will get all index's which are occured once
-            // but our answer is lowest index
-            int index = s.indexOf(c);
-            if(index!=-1&&index==s.lastIndexOf(c)){
-                ans = Math.min(ans,index);
-            }
+    public static Map<Integer,Integer> count(String s)
+    {
+        Map<Integer,Integer> M=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            int c=s.charAt(i);
+            M.put(c,M.getOrDefault(c,0)+1);
+        }
+        return M;
+    }
+    public int firstUniqChar(String s) {
+        Map<Integer,Integer> M=count(s);
+        for(int i=0;i<s.length();i++)
+        {
+            int c=s.charAt(i);
+            if(M.get(c)==1)
+                return i;
         }
 
-        // If ans remain's Integer.MAX_VALUE then their is no unique character
-        return ans==Integer.MAX_VALUE?-1:ans;
-    }   
+        return -1;
+
+        
+    }
 }
