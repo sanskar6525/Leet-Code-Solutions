3c66e00d6b9926ef38ef2e8432a70e40b77df9d9
Sanskar Goel <sanskargoel6@gmail.com>
2024-02-06 08:48:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.8 MB (95.21%)

--- my-folder/0141-linked-list-cycle/solution.java
+++ my-folder/0141-linked-list-cycle/solution.java
@@ -11,26 +11,26 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow=head;
-        if(head==null || head.next==null)
+         if(head == null || head.next == null){
             return false;
-        else
-        {
-            ListNode fast=head.next;
-            while(fast!=slow)
-            {
-            
-            if (fast == null || fast.next == null) {
-                return false;
-            }
-                slow=slow.next;
-                fast=fast.next.next;
-            }
-            return true; 
         }
         
-
-
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if( slow == fast){
+                return true;
+            }
+        }
+        
+        return false;
         
     }
 }
+
+
+
+
