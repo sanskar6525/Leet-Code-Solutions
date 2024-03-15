522070f036c04acd57ad7d07e2a270a8afdbd93c
Sanskar Goel <sanskargoel6@gmail.com>
2024-03-15 04:52:15 +0000

    [LeetCode Sync] Runtime - 21 ms (5.37%), Memory - 54.8 MB (97.66%)

--- my-folder/0238-product-of-array-except-self/solution.java
+++ my-folder/0238-product-of-array-except-self/solution.java
@@ -1,17 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int result[] = new int[n];
-        result[0] = 1;
-        for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
+        int[] pre=new int[nums.length];
+        int[] suf=new int[nums.length];
+        int[] res=new int[nums.length];
+        Arrays.fill(pre,1);
+        Arrays.fill(suf,1);
+        for(int i=1;i<nums.length;i++)
+        {
+            pre[i]=pre[i-1]*nums[i-1];
         }
-        int suffixProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= suffixProduct;
-            suffixProduct *= nums[i];
-        }
+        System.out.println(Arrays.toString(pre));
 
-        return result;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            suf[i]=suf[i+1]*nums[i+1];
+            res[i]=suf[i]*pre[i];
+        }
+        System.out.println(Arrays.toString(suf));
+        res[0]=suf[0];
+        res[nums.length-1]=pre[nums.length-1];
+        return res;
     }
 }
