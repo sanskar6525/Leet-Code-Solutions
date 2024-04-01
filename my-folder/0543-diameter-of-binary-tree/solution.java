709913145d36e29d3649ef27c24940a14d2b7b82
Sanskar Goel <sanskargoel6@gmail.com>
2024-04-01 05:49:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (66.35%)

--- my-folder/0543-diameter-of-binary-tree/solution.java
+++ my-folder/0543-diameter-of-binary-tree/solution.java
@@ -1,21 +1,34 @@
-public class Solution {
-    int diameter;
-
-    public int diameterOfBinaryTree(TreeNode root) {
-        diameter = 0;
-        helper(root);
-        return diameter;
-    }
-
-    private int helper(TreeNode root) {
-        if (root == null) {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter=0;
+    public int height(TreeNode r)
+    {
+        if(r==null)
             return 0;
-        }
+        int h1=height(r.left);
+        int h2=height(r.right);
+        int dia=h1+h2+1;
+        diameter=Math.max(dia,diameter);
+        return 1+Math.max(h1,h2);
 
-        int left = helper(root.left);
-        int right = helper(root.right);
-        diameter = Math.max(diameter, left + right);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter-1;
         
-        return Math.max(left, right) + 1;
     }
 }
