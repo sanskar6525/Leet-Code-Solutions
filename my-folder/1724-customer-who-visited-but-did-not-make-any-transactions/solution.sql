3b466dda548d4031fad79139f95d4502c2232a97
Sanskar Goel <sanskargoel6@gmail.com>
2024-04-01 18:45:22 +0000

    [LeetCode Sync] Runtime - 2405 ms (60.62%), Memory - 0B (100.00%)

---
+++ my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,11 @@
+
+select customer_id , COUNT(VISIT_ID) AS count_no_trans
+from VISITS 
+where 
+  visit_id NOT IN (
+      SELECT 
+         VISIT_ID 
+     FROM TRANSACTIONS
+  )
+  GROUP BY 
+  CUSTOMER_ID ;
