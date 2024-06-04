74c5f6590ae2134103f0c327f65c64e68a4c3789
Sanskar Goel <sanskargoel6@gmail.com>
2024-06-04 12:14:25 +0000

    [LeetCode Sync] Runtime - 5 ms (69.65%), Memory - 42.4 MB (5.40%)

--- my-folder/0409-longest-palindrome/solution.java
+++ my-folder/0409-longest-palindrome/solution.java
@@ -1,15 +1,28 @@
-class Solution {
+public class Solution {
     public int longestPalindrome(String s) {
-        int[] count = new int[128];
-        for (char c: s.toCharArray())
-            count[c]++;
-
-        int ans = 0;
-        for (int v: count) {
-            ans += v / 2 * 2;
-            if (ans % 2 == 0 && v % 2 == 1)
-                ans++;
+        // Initialize a set to keep track of characters with odd frequencies
+        HashSet<Character> charSet = new HashSet<>();
+        // Initialize the length of the longest palindrome
+        int length = 0;
+        
+        // Iterate over each character in the string
+        for (char c : s.toCharArray()) {
+            // If the character is already in the set, remove it and increase the length by 2
+            if (charSet.contains(c)) {
+                charSet.remove(c);
+                length += 2;
+            } else {
+                // If the character is not in the set, add it to the set
+                charSet.add(c);
+            }
         }
-        return ans;
+        
+        // If there are any characters left in the set, add 1 to the length for the middle character
+        if (!charSet.isEmpty()) {
+            length += 1;
+        }
+        
+        // Return the total length of the longest palindrome
+        return length;
     }
 }
