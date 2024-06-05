89193d23402e7aff87e420eff319ca16b520e5bc
Sanskar Goel <sanskargoel6@gmail.com>
2024-06-05 04:35:50 +0000

    [LeetCode Sync] Runtime - 11 ms (20.53%), Memory - 45.3 MB (5.50%)

---
+++ my-folder/1044-find-common-characters/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        List<String> res=new ArrayList<>();
+        for(char ch='a';ch<='z';ch++)
+        {
+            int minCount=Integer.MAX_VALUE;
+
+            for(String word:words)
+            {
+                int count=0;
+                for(char c:word.toCharArray())
+                {
+                    if(c==ch)
+                        count++;
+                }
+                minCount=Math.min(minCount,count);
+            }
+            for(int i=0;i<minCount;i++)
+                res.add(""+ch);
+        }
+        return res;
+    }
+}
