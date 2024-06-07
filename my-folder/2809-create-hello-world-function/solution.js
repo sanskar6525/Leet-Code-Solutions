9aa34d9107ab24e6ac9c0abc859f3e5a5dd46c79
Sanskar Goel <sanskargoel6@gmail.com>
2024-06-07 21:21:52 +0000

    [LeetCode Sync] Runtime - 51 ms (52.53%), Memory - 49 MB (11.20%)

---
+++ my-folder/2809-create-hello-world-function/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+function createHelloWorld() {
+    const greeting = "Hello World";
+    
+    return function()  {
+        return greeting;
+    };
+}
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
