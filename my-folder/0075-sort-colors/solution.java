1befac2c9f34ffb2d13244034f16bb2cc9ed230c
Sanskar Goel <sanskargoel6@gmail.com>
2024-06-12 05:15:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.9 MB (16.27%)

---
+++ my-folder/0075-sort-colors/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            while (i >= l && i <= r && (nums[i] == 0 || nums[i] == 2)) {
+                if (nums[i] == 0) {
+                    swap(nums, i, l);
+                    l++;
+                } else if (nums[i] == 2) {
+                    swap(nums, i, r);
+                    r--;
+                }
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
